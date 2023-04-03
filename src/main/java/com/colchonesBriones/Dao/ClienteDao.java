99dcb2aef5420da8d3dfb5de722e4509859f0540
nicki1002 <nicole18hernandez@hotmail.com>
2023-04-03 17:18:41 -0600

    Implementacion de cliente

--- src/main/java/com/colchonesBriones/Dao/ClienteDao.java
+++ src/main/java/com/colchonesBriones/Dao/ClienteDao.java
@@ -2,11 +2,15 @@
 package com.colchonesBriones.Dao;
 
 import com.colchonesBriones.Domain.Cliente;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 
 
 public interface ClienteDao extends CrudRepository<Cliente, Long>{
-    
+      public List<Cliente> findByApellidos(String apellidos);
+
 }
+
+
 
